a53d0e688606a2460a1504c84e50f86aa1332a8d
madd802 <maddalenaventurini1@gmail.com>
2025-05-27 10:36:14 +0000

    feat: create new request, add route, update component with data from service

--- src/Frontend/src/pages/CustomerListPage.tsx
+++ src/Frontend/src/pages/CustomerListPage.tsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+type CustomersListQueryResponseCategory = {
+code: string;
+description: string;
+}
+type CustomersListQueryResponse = {
+  id: number;
+  name: string;
+  address: string;
+  email: string;
+  phone: string;
+  iban: string;
+  category?: CustomersListQueryResponseCategory;
+}
 export default function CustomerListPage() {
+      const [list, setList] = useState<CustomersListQueryResponse[]>([]);
+    useEffect(() => {
+        fetch("/api/customers/list")
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            setList(data as CustomersListQueryResponse[]);
+          });
+      }, []);
     return <>
-        <i>Todo customers</i>
+        <div>{JSON.stringify(list)}</div>
     </>
 }
